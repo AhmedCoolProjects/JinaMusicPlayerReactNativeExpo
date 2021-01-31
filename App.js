5b11ac5e677e387f3c32ca90498694806e6e6d5e
Ahmed BARGADY <bargadyahmed2000@gmail.com.com>
2021-01-31 23:13:18 +0100

    this is the basic env ready to be built for the frontend.

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import HomePage_ from './app/screens/HomePage_';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <HomePage_ />
       <StatusBar style="auto" />
-    </View>
+      </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ app/screens/HomePage_.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function HomePage_() {
+    return (
+        <View>
+            <Text>thanks GOD</Text>
+        </View>
+    )
+}
